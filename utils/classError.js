bab7c5dfe0961e617aeb83af3c7962e1b7d8258a
Aramis jaime <you@example.com>
2023-07-02 20:40:17 -0300

    i dont know

--- utils/classError.js
+++ utils/classError.js
@@ -9,4 +9,16 @@ class DropError {
   } 
  }
 
- module.exports = DropError
+ const throwError = ()=> {
+  throw new DropError({
+    message:"Error from server, please try again later",
+    status: 500,
+    succes:false,
+    typeError:"Server error"
+  })
+}
+
+ module.exports = {
+  throwError,
+  DropError
+ }
