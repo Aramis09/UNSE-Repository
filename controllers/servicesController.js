4f7850a1836261e209b2546de22f6ee7cfde330a
Aramis jaime <you@example.com>
2023-07-08 20:13:27 -0300

    changes i dont know

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,13 +1,26 @@
 const catchedAsyncErrors = require("../utils/catchedAsyncErrors")
-const {createNewServiceHelper} = require ("../helpers/servicesHelper")
+const {createNewServiceHelper,getServicesFromDbHelper} = require ("../helpers/servicesHelper")
 const {throwError} = require("../utils/classError")
+
+
 const createdNewService = async (req,res) => {
   const succesProcess = await createNewServiceHelper(req.body)
   if(!succesProcess )throwError()
   return res.status(200).send(succesProcess)
 }
 
+const getServices = async (req,res) => {
+  const {page} = req.query
+  const services = await getServicesFromDbHelper(page)
+  console.log(services,"-----------------");
+  if(!services.length )throwError()
+  return res.status(200).send(services)
+}
+
+
 module.exports = {
-  createdNewService:catchedAsyncErrors(createdNewService)
+  createdNewService:catchedAsyncErrors(createdNewService),
+  getServices:catchedAsyncErrors(getServices)
+
 }
 
