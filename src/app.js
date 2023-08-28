863223ea454d2172ac6e5fdf6bb86458eb27f665
Aramis jaime <you@example.com>
2023-08-28 16:53:30 -0300

    try deploy

--- src/app.js
+++ src/app.js
@@ -17,14 +17,14 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser({ credentials: true }));
 server.use(cors({
-  origin:origins,
+  origin:origins[0],
   credentials:true
 }));
 
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', origins); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', origins[0]); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept','auth-token','auth-secret-key');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
