a1ba01c908e4ba556eb253369f953de401870be9
Aramis jaime <you@example.com>
2023-09-10 04:03:36 -0300

    changes

--- src/routes/subRoutes/aboutRoute.js
+++ src/routes/subRoutes/aboutRoute.js
@@ -1,10 +1,11 @@
 const router = require("express");
 const { getAbout,createAbout,editAbout } = require("../../../controllers/aboutController");
+const middlewares = require("../../../middlewares/exports")
 
 
 aboutRouter = router()
-aboutRouter.get("/getAbout",getAbout);
-aboutRouter.post("/createAbout",createAbout);
-aboutRouter.put("/editAbout",editAbout);
+aboutRouter.get("/getAbout" ,getAbout); 
+aboutRouter.post("/createAbout",middlewares.jwtVerify,createAbout); //!falta verificar los datos del cliente
+aboutRouter.put("/editAbout",middlewares.jwtVerify,editAbout);//!falta verificar los datos del cliente
 
 module.exports = aboutRouter
